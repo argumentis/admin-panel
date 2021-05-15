39ad9be7ea89510a04a5f1bd1218a5069b136315
argumentis <argumentishe@gmail.com>
2021-05-15 20:30:08 +0300

    added create Customers

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Dashboard from "../components/dashboard";
-import MainLayout from "../../layout";
+import MainLayout from "../layout/index";
 
 export default function DashboardPage() {
   return (
