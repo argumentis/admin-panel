a310eff87d957c2b21359ee4ed0bcce97bf4c2d9
argumentis <argumentishe@gmail.com>
2021-05-15 19:37:06 +0300

    test2

--- pages/customers/create/index.js
+++ pages/customers/create/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import CreateCustomer from "../../../components/CreateCustomer";
-import MainLayout from "../../layout";
+import MainLayout from "../../../layout/index";
 
 export default function CustomersPage() {
   return <MainLayout />;
